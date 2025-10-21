fe34b16485917765f09492098a192df8d7b61b74
TUDONG05 <khactu731@gmail.com>
2025-10-21 16:51:38 +0700

    up

--- models/userModel.js
+++
@@ -1,9 +0,0 @@
-// models/userModel.js
-const db = require('../config/db');
-
-async function findByUsername(username) {
-  const [rows] = await db.query('SELECT id, username, password FROM users WHERE username = ?', [username]);
-  return rows[0] || null;
-}
-
-module.exports = { findByUsername };
